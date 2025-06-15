a05c6a158f795a8925aa252aa1bedc7c2b48e495
JIA0010 <HIROsroutine@gmail.com>
2025-06-15 14:46:16 +0900

    implement result_search feature before adapting to frontend

--- services/api/result_search/srcs/auth/auth.guard.ts
+++ services/api/result_search/srcs/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class AuthGuard extends PassportAuthGuard('jwt') {}
